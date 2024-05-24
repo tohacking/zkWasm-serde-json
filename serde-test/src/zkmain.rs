0d71a3487d92d6b3e579396a2204a8e303956e54
tohacking <152132683+tohacking@users.noreply.github.com>
2024-05-23 17:00:16 -0700

    feat: try out codec

--- serde-test/src/zkmain.rs
+++ serde-test/src/zkmain.rs
@@ -1,22 +1,68 @@
-use serde::Deserialize;
 use wasm_bindgen::prelude::*;
 use zkwasm_rust_sdk::wasm_input;
 use zkwasm_rust_sdk::wasm_output;
 
-#[derive(Deserialize, Debug)]
-struct User {
-    fingerprint: String,
-    location: String,
+// #[derive(Deserialize, Debug)]
+// struct User {
+//     fingerprint: String,
+//     location: String,
+// }
+
+use parity_scale_codec::Decode;
+use parity_scale_codec::Encode;
+#[cfg(not(feature = "derive"))]
+use parity_scale_codec_derive::Decode;
+#[cfg(not(feature = "derive"))]
+use parity_scale_codec_derive::Encode;
+
+#[derive(Debug, PartialEq, Encode, Decode, Clone, Copy)]
+enum EnumType {
+    #[codec(index = 15)]
+    A,
+    B(u32, u64),
+    C {
+        a: u32,
+        b: u64,
+    },
 }
 
-pub fn dummy_json_operation() {
-    let j = "
-    {
-        \"fingerprint\": \"0xF9BA143B95FF6D82\",
-        \"location\": \"Menlo Park, CA\"
-    }";
+// pub fn dummy_json_operation() {
+//     let j = "
+//     {
+//         \"fingerprint\": \"0xF9BA143B95FF6D82\",
+//         \"location\": \"Menlo Park, CA\"
+//     }";
 
-    let u: User = serde_json::from_str(j).unwrap();
+//     let u: User = serde_json::from_str(j).unwrap();
+// }
+
+pub fn dummy_codec_operation() {
+    let a = EnumType::A;
+    let b = EnumType::B(1, 2);
+    let c = EnumType::C { a: 1, b: 2 };
+
+    a.using_encoded(|ref slice| {
+        assert_eq!(slice, &b"\x0f");
+    });
+
+    b.using_encoded(|ref slice| {
+        assert_eq!(slice, &b"\x01\x01\0\0\0\x02\0\0\0\0\0\0\0");
+
+        let decoded_b = EnumType::decode(&mut &slice[..]).ok().unwrap();
+        assert_eq!(decoded_b, EnumType::B(1, 2));
+
+        if let EnumType::B(first, second) = decoded_b {
+            // Use the first and second items here
+            unsafe {
+                zkwasm_rust_sdk::dbg!("b.0: {}\n", first);
+                zkwasm_rust_sdk::dbg!("b.1: {}\n", second);
+            }
+        }
+    });
+
+    c.using_encoded(|ref slice| {
+        assert_eq!(slice, &b"\x02\x01\0\0\0\x02\0\0\0\0\0\0\0");
+    });
 }
 
 #[wasm_bindgen]
@@ -24,7 +70,8 @@ pub fn zkmain() -> i64 {
     // specify the public inputs
     // let total_steps: u64 = unsafe { wasm_input(1) };
     // let current_position: u64 = unsafe { wasm_input(1) };
-    dummy_json_operation();
+    // dummy_json_operation();
+    dummy_codec_operation();
     // let init_param = RustInitializationParameters {
     //     total_steps,
     //     current_position,
